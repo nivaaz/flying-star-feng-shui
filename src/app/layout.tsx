43ca054f046fd2f7b011cc6fa255543787445cea
nivaaz <nksehmbhi@gmail.com>
2025-02-11 16:21:16 +1100

    fix width of layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import Link from "next/link";
 
 const openSans = Open_Sans({ weight: "400", subsets: ["latin"] });
 
@@ -20,11 +19,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={openSans.className}>
         <>
-          <nav className="text-center text-xs bg-cyan-50 dark:bg-cyan-800  flex flex-row justify-center p-1 ">
-            <p className="my-auto"> Tools by Nivaaz</p>
-     
+          <nav className="text-center text-xs bg-cyan-50 dark:bg-cyan-800 flex flex-row justify-center p-1 ">
+            <p className="my-auto"> Tools by Nivaaz </p>
           </nav>
-          {children}
+          <div className="w-full m-auto p-4 min-h-screen">
+            {children}
+          </div>
         </>
       </body>
       <GoogleAnalytics gaId="G-S4DN9BVZ37" />
