70656121b6c1430d074f0b9ac248bc89f5eb917a
nivaaz <nksehmbhi@gmail.com>
2025-11-03 12:17:20 +1100

    add astro

---
+++ src/app/astro/astrocartography.tsx
@@ -0,0 +1,30 @@
+const corePlanets = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune",
+  "Pluto",
+  "Moon",
+  "Sun",
+  "Chiron",
+  "Lilith",
+];
+const maleficPlanets = ["Saturn", "Mars", "Pluto", "Mars", "Neptune", "Uranus"];
+const beneficPlanets = ["Jupiter", "Venus", "Moon", "Sun"];
+const angles = ["ASC", "DSC", "MC", "IC"]; // TODO: check if this needs to be spelled out completely.
+// TODO: add something about sects?
+
+// create a plan for benefits
+
+// create a plan for malefics
+
+// what rising to move to, to cure it.
+
+export const Astrocartography = () => {
+// understand 
+  return <div> Astrocartography Works!</div>;
+};
